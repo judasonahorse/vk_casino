d6aa7735f2d05c1038f331f13215785e20b56a48
Кирилл Нетёсов <77106679+judasonahorse@users.noreply.github.com>
2021-08-27 00:51:18 +0300

    Update sqlighter.py

--- sqlighter.py
+++ sqlighter.py
@@ -16,6 +16,9 @@ class SQLighter:
     def get_balance(self, user_id,chat_id):
         with self.connection:
             return self.cursor.execute(f'SELECT balance FROM `{chat_id}` WHERE `user_id` = ?', (user_id,)).fetchall()
+
+
+
     def get_bank(self, user_id,chat_id):
         with self.connection:
             return self.cursor.execute(f'SELECT bank FROM `{chat_id}` WHERE `user_id` = ?', (user_id,)).fetchall()
@@ -27,15 +30,18 @@ class SQLighter:
     def get_time_bonus(self, user_id,chat_id):
         with self.connection:
             return self.cursor.execute(f'SELECT time_bonus FROM `{chat_id}` WHERE `user_id` = ?', (user_id,)).fetchall()
-
-
+    def get_time_bank_procent(self, user_id,chat_id):
+        with self.connection:
+            return self.cursor.execute(f'SELECT bank_procent FROM `{chat_id}` WHERE `user_id` = ?', (user_id,)).fetchall()
     def get_medal(self, user_id,chat_id):
         with self.connection:
             return self.cursor.execute(f'SELECT medal FROM `{chat_id}` WHERE `user_id` = ?', (user_id,)).fetchall()
     def get_top(self,chat_id):
         with self.connection:
             return self.cursor.execute(f'SELECT * FROM `{chat_id}` ORDER BY `medal` DESC , `balance` +`bank` DESC' , ()).fetchall()
-
+    def get_last_game_id(self, game_id):
+        with self.connection:
+            return self.cursor.execute(f'SELECT * FROM `{game_id}` ORDER BY `game_id` DESC', ()).fetchall()
     def subscriber_exists(self, user_id,chat_id):
         """Проверяем, есть ли уже юзер в базе"""
         with self.connection:
@@ -47,7 +53,9 @@ class SQLighter:
         with self.connection:
             return self.cursor.execute(f"INSERT INTO `{chat_id}` (`user_id`, `status`) VALUES(?,?)", (user_id, status))
 
-
+    def add_new_game(self, user_id, game_id,status=True):
+        with self.connection:
+            return self.cursor.execute(f"INSERT INTO `{game_id}` (`game_id`, `status`) VALUES(?,?)", (user_id, status))
     def update_balance(self, user_id,status,chat_id):
         with self.connection:
             return self.cursor.execute(f"UPDATE `{chat_id}` SET `balance` = ? WHERE `user_id` = ?", (status, user_id))
@@ -57,6 +65,9 @@ class SQLighter:
     def update_medal(self, user_id,status,chat_id ):
         with self.connection:
             return self.cursor.execute(f"UPDATE `{chat_id}` SET `medal` = ? WHERE `user_id` = ?", (status, user_id))
+    def update_list_game(self, user_id,status,chat_id ):
+        with self.connection:
+            return self.cursor.execute(f"UPDATE `{chat_id}` SET `list_game` = ? WHERE `game_id` = ?", (status, user_id))
 
     def update_bonus(self, user_id, status,chat_id):
         with self.connection:
@@ -66,10 +77,16 @@ class SQLighter:
 
         with self.connection:
             return self.cursor.execute(f"UPDATE `{chat_id}` SET `time_bonus` = ? WHERE `user_id` = ?", (status, user_id))
+    def update_time_bank_procent(self, user_id,status, chat_id):
 
-    def create_table_for_char(self,chat_id):
         with self.connection:
-            return self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {chat_id}(user_id CHAR, status BOOLEAN, first_name CHAR,balance INT,bonus BOOLEAN,time_bonus DATE, medal INT DEFAULT 0 )")
+            return self.cursor.execute(f"UPDATE `{chat_id}` SET `bank_procent` = ? WHERE `user_id` = ?", (status, user_id))
+
+
+    def create_table_for_chat(self,chat_id):
+        with self.connection:
+            return self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {chat_id}(user_id INT, status BOOLEAN, first_name CHAR,balance INT,bonus BOOLEAN,time_bonus DATE, medal INT DEFAULT 0,bank INT DEFAULT 0,bank_procent DATE DEFAULT 0)")
+
 
     def close(self):
         """Закрываем соединение с БД"""
